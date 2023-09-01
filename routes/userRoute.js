3bb1be91a65cfca66431663877b56b79cf3654ea
Eduard Metzler <eduard.metzler@mail.ru>
2023-09-01 20:14:35 +0200

    added clearCookie

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,7 +4,12 @@ import {
   validate,
 } from "../middleware/userValidation.js";
 
-import { register, login, checkCookie } from "../controllers/userController.js";
+import {
+  register,
+  login,
+  checkCookie,
+  clearCookie,
+} from "../controllers/userController.js";
 import { authenticate } from "../middleware/authenticate.js";
 
 const router = express.Router();
@@ -14,5 +19,6 @@ router.post("/register", registerValidationRules, validate, register);
 router.post("/login", login);
 
 router.get("/checkCookie", authenticate, checkCookie);
+router.get("/clearCookie", clearCookie);
 
 export default router;
